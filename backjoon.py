f4ff5e4fc1bfb00392bb2268fb74b36cde9d86e6
김재빈 <jaebin@gimjaebin-ui-MacBookPro.local>
2021-12-28 22:07:12 +0900

    Stduy

--- backjoon.py
+++ backjoon.py
@@ -250,3 +250,24 @@
 #         else:
 #             dp[i][j] = max(dp[i - 1][j - 1] + dp[i][j], dp[i - 1][j] + dp[i][j])
 # print(max(dp[n - 1]))
+
+#1912
+# n = int(input())
+# l = list(map(int, input().split()))
+# dp = [l[0]]
+# for i in range(len(l) - 1):
+#     dp.append(max(dp[i] + l[i + 1], l[i + 1]))
+# print(max(dp))
+
+#2156
+# n = int(input())
+# wine = [0]
+# dp = [0]
+# for i in range(n):
+#     wine.append(int(input()))
+# dp.append(wine[1])
+# if n > 1:
+#     dp.append(wine[1] + wine[2])
+# for i in range(3, n + 1):
+#     dp.append(max(dp[i - 1], dp[i - 3] + wine[i - 1] + wine[i], dp[i - 2] + wine[i]))
+# print(dp[n])
